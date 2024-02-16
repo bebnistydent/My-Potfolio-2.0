8c677694a5ae57e05acc57f348800f7a3b4914db
Nikita Yerameev <Smertskosoii@gmail.com>
2024-02-16 17:35:38 +0300

    Did 'Main part 3' from week 3

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,7 @@ import photo from "./../../../assets/images/PhotoOfMine.webp"
 import { FlexBox } from "../../../assets/components/conteiner/Flex.box";
 import { Conteiner } from "../../../assets/components/conteiner/Conteiner";
 import { theme } from "../../../Styles/Theme";
+import { font } from "../../../Styles/Common";
 
 
 export const Main = () => {
@@ -11,7 +12,7 @@ export const Main = () => {
         <StyledMain>
             <Conteiner> 
 
-         <FlexBox align={"center"} justify={"space-between"}> 
+         <FlexBox align={"center"} justify={"space-around"} wrap={"wrap"}> 
             <div>
                  <SmallText>Hi There</SmallText>
                  <MyName>I am <span>Nikita Yerameyeu</span></MyName>
@@ -40,6 +41,7 @@ const StyledMain = styled.section `
 const PhotoLine = styled.div`
     position: relative;
     z-index: 1;
+    margin-top: 65px;
 
     &::before{
         content: "";
@@ -51,6 +53,12 @@ const PhotoLine = styled.div`
         top: -24px;
         left: 24px;
         z-index: -1;
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+            top: -17px;
+            left: 20px;
+        }
     }
 `
 
@@ -58,24 +66,33 @@ const Photo = styled.img `
     width: 320px;
     height: 400px;
     object-fit: cover;
+    margin-right: 20px;
+
+    @media ${theme.media.mobile} {
+        width: 310px;
+        height: 380px;
+
+        @media ${theme.media.mobile} {
+            width: 314px;
+            height: 414px;
+        }
+    }
 
 `
 const MainTitle = styled.h1 `
-    font-weight: 400;
-    font-size: 27px;
+    ${font({weight: 400, Fmax: 27, Fmin: 20})}
 
 `
 
 const MyName = styled.h2`
-    font-family: 'Josefin Sans', sans-serif;
-    font-weight: 700;
-    font-size: 50px;
+    ${font({family: "'Poppins', sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
     letter-spacing: 0.05rem;
     margin: 10px 0; 
 
     span {
          position: relative;
          z-index: 1;
+         white-space: nowrap;
 
         &::before {
             content: "";
@@ -90,9 +107,14 @@ const MyName = styled.h2`
         }
     }
 
+@media ${theme.media.mobile} {
+margin: 15px 0 22px;
+}
+
 `
 const SmallText = styled.h2`
 font-weight: 400;
 font-size: 14px;
+
 `
 
