aa6c89f15e3525565e18397d0a99ee32582a0486
Nikita Yerameev <Smertskosoii@gmail.com>
2024-02-27 12:54:49 +0300

    Did 'Burger Menu' video from week 4

--- src/layout/header/headerMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/MobileMenu.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Menu } from "./Menu";
 import { S } from "./Header.Menu_Styles";
 
 export const ModileMenu: React.FC<{ menuItems: Array<string> }> = (props: { menuItems: Array<string> }) => {
+ 
+  const [menuIsOpen, setmenuIsOpen] = useState(false)
+  const onBurgerBtnClick = () => {setmenuIsOpen(!menuIsOpen)}
+ 
   return (
     <S.MobileMenu>
-      <S.BurgerButton isOpen={false}>
+      <S.BurgerButton isOpen={menuIsOpen} onClick={onBurgerBtnClick}>
         <span></span>
       </S.BurgerButton>
 
-      <S.MobileMenuPopup isOpen={false}>
+      <S.MobileMenuPopup isOpen={menuIsOpen} onClick={()=>{setmenuIsOpen(false)}}>
         <Menu menuItems={props.menuItems} />
       </S.MobileMenuPopup>
     </S.MobileMenu>
   );
 };
+
+
 
